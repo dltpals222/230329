048a81c2181a7fde24876b1596c84bc59ef0e827
leesemin <leesemin222@gmail.com>
2023-03-29 12:11:41 +0900

    불편함을 느껴봐라

---
+++ itsme.js
@@ -0,0 +1,30 @@
+const itsMe = {
+  firstName : "lee",
+  lastName : "semin",
+  age : "32",
+  email : "leesemin@gmail.com",
+  phone : "010-1234-5678",
+  address : "대전광역시에 살아요",
+  like : "game",
+  hate : "tired",
+  hobby : "game",
+  coffee : "아메리카노",
+  food : "전부다!",
+  sleepTime : "8시간",
+  likeColor : "빨주노초파남보",
+}
+
+function gaeIn (firstName, lastName, age, email, address, like, hate, hobby, coffee, food, sleepTime, likeColor) {
+  this.firstName = firstName
+  this.lastName = lastName
+  this.age = age
+  this.email = email
+  this.address = address
+  this.like = like
+  this.hate = hate
+  this.hobby = hobby
+  this.coffee = coffee
+  this.food = food
+  this.sleepTime = sleepTime
+  this.lilikeColor =likeColor
+}
